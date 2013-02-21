ebabfcbec6f67618affeb5dacb9203821c68ba11
Richard Jones <itszootime@gmail.com>
2013-02-21 14:36:21 +0000

    Add tests for GeoFeature

--- src/test/java/me/itszooti/geojson/GeoFeatureTest.java
+++ src/test/java/me/itszooti/geojson/GeoFeatureTest.java
@@ -2,14 +2,42 @@ package me.itszooti.geojson;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
 
 import org.junit.Test;
 
 public class GeoFeatureTest {
 
+	private GeoFeature feature;
+	
+	@Before
+	public void before() {
+		feature = new GeoFeature("a_test_feature", new GeoPoint(new GeoPosition(100.0, 0.0)));
+		feature.setProperty("number", 2);
+		feature.setProperty("letter", "B");
+	}
+	
 	@Test
-	public void test() {
-		assertThat(true, equalTo(false));
+	public void getID() {
+		assertThat(feature.getID(), equalTo("a_test_feature"));
+	}
+	
+	@Test
+	public void getGeometry() {
+		GeoGeometry geometry = feature.getGeometry();
+		assertThat(geometry, notNullValue());
+		assertThat(geometry, instanceOf(GeoPoint.class));
+	}
+	
+	@Test
+	public void getProperty() {
+		assertThat(feature.getProperty("number"), equalTo(2));
+		assertThat(feature.getProperty("letter"), equalTo("B"));
+	}
+	
+	@Test
+	public void isGeoObject() {
+		assertThat(feature, instanceOf(GeoObject.class));
 	}
 	
 }
