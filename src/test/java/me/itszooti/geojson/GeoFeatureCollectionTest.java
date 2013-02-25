8c636613eb6785b1b20eb3d0b50fe73163a7b1db
Richard Jones <itszootime@gmail.com>
2013-02-25 12:34:50 +0000

    Tests for FeatureCollection

--- src/test/java/me/itszooti/geojson/GeoFeatureCollectionTest.java
+++ src/test/java/me/itszooti/geojson/GeoFeatureCollectionTest.java
@@ -6,9 +6,36 @@ import static org.hamcrest.MatcherAssert.*;
 
 public class GeoFeatureCollectionTest {
 
+	private GeoFeatureCollection featureColl;
+	
+	@Before
+	public void before() {
+		GeoFeature[] features = new GeoFeature[] {
+			new GeoFeature("a_test_feature", new GeoPoint(new GeoPosition(100.0, 0.0))),
+			new GeoFeature("another_test_feature", new GeoPoint(new GeoPosition(0.0, 100.0)))
+		};
+		featureColl = new GeoFeatureCollection(Arrays.asList(features));
+	}
+	
 	@Test
-	public void test() {
-		assertThat(true, equalTo(false));
+	public void getNumFeatures() {
+		assertThat(featureColl.getNumFeatures(), equalTo(2));
+	}
+	
+	@Test
+	public void getFeature() {
+		// get first
+		GeoFeature feature1 = featureColl.getFeature(0);
+		assertThat(feature1.getID(), equalTo("a_test_feature"));
+		
+		// get second
+		GeoFeature feature2 = featureColl.getFeature(1);
+		assertThat(feature2.getID(), equalTo("another_test_feature"));
+	}
+	
+	@Test
+	public void isGeoObject() {
+		assertThat(featureColl, instanceOf(GeoObject.class));
 	}
 	
 }
