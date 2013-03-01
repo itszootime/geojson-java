cdc8e53e357e2dd5d0336b28dc2f4dd9b41d9932
Richard Jones <itszootime@gmail.com>
2013-03-01 13:24:59 +0000

    Implement GeoFeature encoding

--- src/main/java/me/itszooti/geojson/gson/GeoFeatureSerializer.java
+++ src/main/java/me/itszooti/geojson/gson/GeoFeatureSerializer.java
@@ -16,6 +16,7 @@ public class GeoFeatureSerializer implements JsonSerializer<GeoFeature> {
 	public JsonElement serialize(GeoFeature feature, Type type, JsonSerializationContext context) {
 		// always an object
 		JsonObject object = new JsonObject();
+		object.addProperty("type", "Feature");
 		
 		// add id
 		object.addProperty("id", feature.getId());
@@ -25,7 +26,11 @@ public class GeoFeatureSerializer implements JsonSerializer<GeoFeature> {
 		object.add("geometry", context.serialize(feature.getGeometry(), GeoObject.class));
 		
 		// add properties
-		
+		JsonObject properties = new JsonObject();
+		object.add("properties", properties);
+		for (String name : feature.getPropertyNames()) {
+			properties.add(name, context.serialize(feature.getProperty(name)));
+		}
 		
 		return object;
 	}
