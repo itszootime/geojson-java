f8af3c9fcbc73043eef1a90307b3ccc8b537fc1f
Richard Jones <itszootime@gmail.com>
2013-03-01 13:18:23 +0000

    Building GeoFeature serializer

--- src/main/java/me/itszooti/geojson/gson/GeoFeatureSerializer.java
+++ src/main/java/me/itszooti/geojson/gson/GeoFeatureSerializer.java
@@ -3,18 +3,31 @@ package me.itszooti.geojson.gson;
 import java.lang.reflect.Type;
 
 import me.itszooti.geojson.GeoFeature;
+import me.itszooti.geojson.GeoObject;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
 public class GeoFeatureSerializer implements JsonSerializer<GeoFeature> {
 
 	@Override
-	public JsonElement serialize(GeoFeature arg0, Type arg1,
-			JsonSerializationContext arg2) {
-		// TODO Auto-generated method stub
-		return null;
+	public JsonElement serialize(GeoFeature feature, Type type, JsonSerializationContext context) {
+		// always an object
+		JsonObject object = new JsonObject();
+		
+		// add id
+		object.addProperty("id", feature.getId());
+		
+		// add geometry
+		// force to go through GeoObject serializer
+		object.add("geometry", context.serialize(feature.getGeometry(), GeoObject.class));
+		
+		// add properties
+		
+		
+		return object;
 	}
 
 	
